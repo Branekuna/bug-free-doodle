947bf8c16e761c94c190452f4b639a99f160f4b4
branimir kunstek <branekuna@gmail.com>
2024-10-07 00:55:15 +0200

    Various improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
+import { CalendarSection } from '@/components/sections/CalendarSection'
 import { Button } from '@/components/ui/button'
 
 const HomePage: React.FC = () => {
-    return (
-        <div>
-            <h1>Welcome to My Next.js Home Page</h1>
-            <Button variant="default">Click meee</Button>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Welcome to My Next.js Home Page</h1>
+      <Button variant="default">Click meee</Button>
+      <CalendarSection />
+    </div>
+  )
 }
 
 export default HomePage
---
+++ components/sections/CalendarSection.tsx
@@ -0,0 +1,12 @@
+import { Calendar } from '@/components/ui/calendar'
+
+export const CalendarSection: React.FC = () => {
+    return (
+        <>
+            <div>
+                <h1>Calendar Section</h1>
+            </div>
+            <Calendar />
+        </>
+    )
+}
